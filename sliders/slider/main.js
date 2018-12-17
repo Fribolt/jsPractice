bcc8240b473139ca7e6aaf852b418a2e6609a172
Vlad Pribilnov <vladp@leadsmarket.com>
2018-12-17 20:29:58 +0200

    refactoring slider

--- sliders/slider/main.js
+++ sliders/slider/main.js
@@ -3,23 +3,26 @@ const nextButton = document.querySelector('#next');
 const prevButton = document.querySelector('#prev');
 let currentSlideNumber = 1;
 
-//          Листнер должен вызывать функцию - в которой будет реализация next()
-nextButton.addEventListener('click', function () {
-    sliderItems[currentSlideNumber -1].classList.toggle('active-slide');
+const next = () => {
+    switchActiveElement();
     ++currentSlideNumber;
-    //          Условие нужно вынести в метод, например isLast()
-    if (currentSlideNumber >= sliderItems.length) {
-        currentSlideNumber = 1;
-    }
-    //      Тоглы выглядят как дулирование кода- вынести в функцию
-    sliderItems[currentSlideNumber -1].classList.toggle('active-slide');
-});
-//          Ситуация повторяется)
-prevButton.addEventListener('click', function () {
-    sliderItems[currentSlideNumber -1].classList.toggle('active-slide');
+    isLast();
+    switchActiveElement();
+};
+
+const prev = () => {
+    switchActiveElement();
     --currentSlideNumber;
-    if (currentSlideNumber <= 1) {
-        currentSlideNumber = sliderItems.length;
-    }
+    isFirst();
+    switchActiveElement();
+};
+
+const switchActiveElement = () => {
     sliderItems[currentSlideNumber -1].classList.toggle('active-slide');
-});
+};
+
+const isLast = () => currentSlideNumber > sliderItems.length ? currentSlideNumber = 1 : false;
+const isFirst = () => currentSlideNumber < 1 ? currentSlideNumber = sliderItems.length : false;
+
+nextButton.addEventListener('click', next);
+prevButton.addEventListener('click', prev);
