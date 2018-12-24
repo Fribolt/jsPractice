e850dc00e0186b1375dbfed6f20eaaecdbe1717c
Vlad Prybylnov <pribilnov97@gmail.com>
2018-12-24 02:17:00 +0200

    done

--- date-picker/main.js
+++ date-picker/main.js
@@ -1,6 +1,14 @@
-let dateNow = new Date();
-let currentDate = document.querySelector('#current-date');
-let dateOptions = {
+let counter = 0;
+let selectDate = new Date();
+const dateNow = new Date();
+const currentDate = document.querySelector('#current-date');
+const modifyDate = document.querySelector('#modify-date');
+const differenceDate1 = document.querySelector('#difference-date');
+const select = document.querySelector('#select-date');
+const changeDateButton = document.querySelector('#change-date');
+const compareDateButton = document.querySelector('#compare-date');
+
+const dateOptions = {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
@@ -8,50 +16,79 @@ let dateOptions = {
     minute: 'numeric',
     second: 'numeric'
 };
-
-let setDate = {
-  halfDay: {
-      title: 'Half Day',
-      date: 12
-  },
-  day: {
-      title: 'Day',
-      date: 24
-  },
+const intervals = {
+    halfHour: 1800000,
+    hour: 3600000,
+    day: 86400000,
+    week: 604800000,
+    halfMonth: 1296000000,
+};
+const setDate = {
+    halfHour: {
+        title: 'Half an Hour',
+        date: intervals.halfHour,
+    },
+    hour: {
+        title: 'Hour',
+        date: intervals.hour,
+    },
+    day: {
+        title: 'Day',
+        date: intervals.day,
+    },
+    week: {
+        title: 'Week',
+        date: intervals.week,
+    },
+    halfMonth: {
+        title: 'Half an Month',
+        date: intervals.halfMonth,
+    },
 };
 
-function createOptions(title) {
+currentDate.innerHTML = dateNow.toLocaleDateString('ru', dateOptions);
+
+const getValue = () => select.value;
+
+const calculateDate = () => selectDate = +selectDate + +getValue();
+
+const showModifyDate = () => {
+    modifyDate.innerHTML = new Date(calculateDate()).toLocaleDateString('ru', dateOptions);
+};
+
+const setOptions = (title) => {
     let select = document.querySelector('#select-date');
     let option = document.createElement('option');
 
     option.innerText = title;
     option.classList.add('option');
     select.appendChild(option);
-}
+};
 
-function setAttribute(date) {
+const setAttribute = (date) => {
     let options = document.querySelectorAll('.option');
-    options.setAttribute('value', date);
-}
+    options[counter].setAttribute('value', date);
+    counter++;
+};
 
-for(let key in setDate) {
+for (let key in setDate) {
     for (let key1 in setDate[key]) {
         if (typeof setDate[key][key1] == "string") {
-            createOptions(setDate[key][key1]);
+            setOptions(setDate[key][key1]);
         }
     }
 }
 
-for(let key in setDate) {
+for (let key in setDate) {
     for (let key1 in setDate[key]) {
         if (typeof setDate[key][key1] == "number") {
-            createOptions(setDate[key][key1]);
+            setAttribute(setDate[key][key1]);
         }
     }
 }
 
-currentDate.innerHTML = dateNow.toLocaleDateString('ru', dateOptions);
-
-function calculetDate(dateNow) {
-    +dateNow
-}
+changeDateButton.addEventListener('click', showModifyDate);
+compareDateButton.addEventListener('click', function () {
+    let differenceDate = selectDate - dateNow;
+    differenceDate1.innerHTML = `Разница в часах: ${differenceDate / 1000 / 60 / 60}`;
+});
