348622f0aedef1edad8246b6e9f8d5f903fd59ff
Vlad Prybylnov <pribilnov97@gmail.com>
2019-01-31 00:49:43 +0200

    init video folder

---
+++ video-player/main.js
@@ -0,0 +1,6 @@
+const videoElement =  document.querySelector('#video');
+const playButton =  document.querySelector('#play-button');
+const puseButton =  document.querySelector('#puse-button');
+
+
+videoElement.autoplay = true;
