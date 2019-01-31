9178c156a87c5a0d41500d54715791540ffa4f19
Vlad Pribilnov <vladp@leadsmarket.com>
2019-01-31 19:49:30 +0200

    add track time for console

--- video-player/main.js
+++ video-player/main.js
@@ -1,6 +1,8 @@
 const videoElement =  document.querySelector('#video');
 const playButton =  document.querySelector('#play-button');
-const puseButton =  document.querySelector('#puse-button');
+const pauseButton =  document.querySelector('#pause-button');
 
 
-videoElement.autoplay = true;
+playButton.addEventListener('click', () => videoElement.play());
+pauseButton.addEventListener('click', () => videoElement.pause());
+videoElement.addEventListener('timeupdate', () => console.log(videoElement.currentTime));
