ac3d4b8791451798b53ed6eef2b7cfa9315d7a02
Vlad Pribilnov <vladp@leadsmarket.com>
2019-01-31 19:57:38 +0200

    add video duration

--- video-player/main.js
+++ video-player/main.js
@@ -1,6 +1,7 @@
 const videoElement =  document.querySelector('#video');
 const playButton =  document.querySelector('#play-button');
 const pauseButton =  document.querySelector('#pause-button');
+const longVideo = videoElement.duration;
 
 
 playButton.addEventListener('click', () => videoElement.play());
