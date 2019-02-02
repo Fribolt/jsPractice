ba24c26a1897cf24f6031aa2b2da671dbf968542
Vlad Prybylnov <pribilnov97@gmail.com>
2019-02-02 15:25:05 +0200

    added progress bar

--- video-player/main.js
+++ video-player/main.js
@@ -1,9 +1,21 @@
 const videoElement =  document.querySelector('#video');
 const playButton =  document.querySelector('#play-button');
 const pauseButton =  document.querySelector('#pause-button');
-const longVideo = videoElement.duration;
+const rewindButton =  document.querySelector('#rewind-button');
+const progressBar =  document.querySelector('#progress-bar');
+let longVideo = null;
 
 
+videoElement.addEventListener('onload', () => console.log(longVideo));
 playButton.addEventListener('click', () => videoElement.play());
 pauseButton.addEventListener('click', () => videoElement.pause());
-videoElement.addEventListener('timeupdate', () => console.log(videoElement.currentTime));
+rewindButton.addEventListener('click', () => videoElement.currentTime = 3);
+videoElement.addEventListener('timeupdate', () => {
+    progressBar.value = videoElement.currentTime;
+});
+
+videoElement.addEventListener('loadedmetadata',() => {
+    longVideo = videoElement.duration;
+    progressBar.setAttribute('step',longVideo * 1 / 100);
+    progressBar.setAttribute('max', longVideo);
+});
